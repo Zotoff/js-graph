0f0e6147ebe4686697566b888179391e549c111c
pavel.zotov <pavel.zotov@axenix.pro>
2025-06-27 09:41:56 +0300

    multi-queries

--- src/resolvers/index.js
+++ src/resolvers/index.js
@@ -1,11 +1,15 @@
+import GraphQLDateTime from 'graphql-iso-date'
 import Query from './query.js'
 import Mutation from './mutation.js'
-import GraphQLDateTime from 'graphql-iso-date'
+import Note from './note.js';
+import User from './user.js';
 
 // ресолверы - функции, которые возвращают данные
 const resolvers = {
   Query,
   Mutation,
+  Note,
+  User,
   DateTime: GraphQLDateTime,
 }
 
